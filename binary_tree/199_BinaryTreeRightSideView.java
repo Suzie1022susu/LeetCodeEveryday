62e08435fd42c28288808a7c8415158159783cbc
Suzie1022susu <susu@omnic.ai>
2023-03-27 21:18:25 -0400

    update

--- binary_tree/199_BinaryTreeRightSideView.java
+++ binary_tree/199_BinaryTreeRightSideView.java
@@ -5,15 +5,25 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
          
          if(root==null) return result;
-         fun1(root);
-        
+         fun1(root); 
     }
 
     public void fun1(TreeNode root){
         Queue<TreeNode> que = new LinkedList<TreeNode>();
         que.offer(root);
         while(!que.isEmpty()){
+            int len = que.size();
+          
+            while(len>0){
+                TreeNode node = que.poll();
+                if(len == 1) result.add(node.val);
+                if(node.left != null) que.offer(node.left);
+                if(node.right != null) que.offer(node.right);
+                len--;
+            }
             
         }
+
+        return result;
     }
 }
